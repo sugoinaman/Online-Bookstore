5d325d4a62bde299d36aadd7c781751886fc80fb
Naman <jhanaman571@gmail.com>
2024-01-13 21:34:35 +0530

    initial commit with oauth
    oauth implemented using google credentials

--- src/main/java/org/example/onlinebookstore/OnlineBookstoreApplication.java
+++ src/main/java/org/example/onlinebookstore/OnlineBookstoreApplication.java
@@ -2,12 +2,24 @@ package org.example.onlinebookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @SpringBootApplication
 public class OnlineBookstoreApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineBookstoreApplication.class, args);
+    }
+
+    @GetMapping("/user")
+    public String user(@AuthenticationPrincipal OAuth2User principal) {
+        System.out.println(principal);
+        return principal.getName();
     }
 
 }
--- src/main/java/org/example/onlinebookstore/SecurityAdapter.java
+++ src/main/java/org/example/onlinebookstore/SecurityAdapter.java
@@ -3,19 +3,38 @@ package org.example.onlinebookstore;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
 
 @Configuration
 public class SecurityAdapter {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http )throws Exception{
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(a->a
-                .requestMatchers(",","/error").permitAll()
-                .
+        http.authorizeHttpRequests(a -> a
+                        .requestMatchers(",", "/error").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .exceptionHandling(e -> e
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+                )
+                .csrf(c -> c
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()) //random thing for angular
+                )
+                .logout(l -> l
+                        .logoutSuccessUrl("/").permitAll()
+                )
 
+                .oauth2Login(
+                        Customizer.withDefaults()
+                ); // to permit login page
+return http.build();
     }
 
 }
