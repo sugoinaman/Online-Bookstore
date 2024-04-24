ea7f34e52d85bc71647035a68af52922f7c62dd5
sugoi <91241514+sugoinaman@users.noreply.github.com>
2024-04-24 11:07:41 +0530

    Wrote tests for BookController

--- src/test/java/org/example/onlinebookstore/book/BookControllerTest.java
+++ src/test/java/org/example/onlinebookstore/book/BookControllerTest.java
@@ -1,33 +1,61 @@
 package org.example.onlinebookstore.book;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.Optional;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(BookController.class)
+
+@WebMvcTest(controllers = BookController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
 class BookControllerTest {
 
-    @Autowired
-    MockMvc mvc;
-
     @MockBean
-    BookRepository bookRepository;
+    private BookRepository repository;
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
 
     @Test
-    @WithMockUser(roles="ADMIN")
-    public void ShouldRun() throws Exception {
+    void shouldReturnABook() throws Exception {
+        Book book = new Book(1, "To Kill a Mockingbird", "Harper Lee", 1960);
+        Mockito.doReturn(Optional.of(book)).when(repository).findById(1);
 
-        MvcResult result = mvc.perform(get("/api/books/1")).andReturn();
-
-        assertEquals(200, result.getResponse().getStatus());
+        final String expectedResponseContent = objectMapper.writeValueAsString(book);
+        this.mvc.perform(get("/api/books/1"))
+                .andExpect(status().isOk())
+                .andExpect(content().json(expectedResponseContent));
     }
+
+    @Test
+    void shouldCreateABook() throws Exception {
+        Book book = new Book(69, "To kill a DockingBird", "Harper Gee", 1969);
+
+        final String uploadContent = objectMapper.writeValueAsString(book);
+        this.mvc.perform(post("/api/books")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(uploadContent))
+                .andExpect(status().isCreated());
+    }
+
 
 }
